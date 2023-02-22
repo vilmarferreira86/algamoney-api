987a0c2e047d8b639509d864791de9f4c239c49d
Vilmar Ferreira <vilmar.jr86@gmail.com>
2023-02-21 22:01:24 -0300

    iniciando projeto algamoney api

---
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -0,0 +1,32 @@
+package com.algaworks.algamoney.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	private String nome;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
